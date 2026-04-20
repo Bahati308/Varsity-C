f01a203f90322042e914be729bd19fc7d2d7ade7
Bahati308 <bahatibrianp@gmail.com>
2026-04-20 21:33:52 +0300

    Add arithmetic

---
+++ arithmetic.c
@@ -0,0 +1,14 @@
+// This program performs basic arithmetic operations: addition, subtraction, multiplication, and division.
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    num1 = 30; // First number
+    num2 = 10;  // Second number
+
+    printf("Sum: %d\n", num1 + num2); //Addition
+    printf("Difference: %d\n", num1 - num2); //Subtraction
+    printf("Product: %d\n", num1 * num2); //Multiplication
+    printf("Quotient: %d\n", num1 / num2); //Division
+    return 0;
+}
