b1c06e85a85e0184ae3c9a8893789d116c684db5
Bahati308 <bahatibrianp@gmail.com>
2026-04-27 12:12:57 +0300

    Added swapping program

---
+++ swap.c
@@ -0,0 +1,17 @@
+// This program swaps values of two integers 
+//without introducing a third variable.
+
+#include <stdio.h>
+
+int main(){
+    int a=2, b=3;
+    printf("Value of a before swap: %d\n", a);
+    printf("Value of b before swap: %d\n", b);
+
+    printf("After swapping we have:\n");
+    a = 6/2; // a = 3
+    b = 6/3; // b = 2
+    printf("Value of a after swap: %d\n", a);
+    printf("Value of b after swap: %d\n", b);
+    return 0;
+}
