6a0c79ec619141c7b3c34a91df24a9508ce3bfb0
Bahati308 <bahatibrianp@gmail.com>
2026-05-04 22:04:43 +0300

    Add for loop control statements

---
+++ forloop.c
@@ -0,0 +1,12 @@
+// This is a simple for loop in C that increaments by 1
+
+#include <stdio.h>
+int i;
+int main(){
+    for (i = 1; i < 20; i++)
+    {
+        printf("%d is less than 20 but greater than 0\n", i);
+    }
+    
+    return 0;
+}
