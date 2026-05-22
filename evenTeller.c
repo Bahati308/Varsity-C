4686baf86491ba01fee4e2eea4911680ceca7346
Bahati308 <bahatibrianp@gmail.com>
2026-05-22 08:54:29 +0300

    Updated temperature converters

---
+++ evenTeller.c
@@ -0,0 +1,22 @@
+// This program determines if a number is even or not
+
+#include <stdio.h>
+
+int evenNumber(int number){
+    if(number <= 1){
+        printf("Number entered aint even");
+        return 0;
+    }
+
+    for (int i = 0; i < number/2; i++)
+    {
+        if (i % == 1)
+        {
+            printf("Number not even");
+            return 0;
+        }
+        
+    }
+    
+ return 0;
+}
---
+++ ifstatements.c
@@ -0,0 +1,24 @@
+// if statements 
+
+#include <stdio.h>
+
+int main(){
+
+    int mark;
+    printf("Enter the marks");
+    scanf("%d", &marks);
+    if (mark <= 40)
+    {
+        printf("See the DOS");
+    }
+
+    else if(mark <= 60){
+        printf("See your mother");
+    }
+
+    else if(mark <= 80){
+        printf("See your father");
+    }
+    
+    return 0;
+}
